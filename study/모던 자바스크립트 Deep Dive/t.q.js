efa71581f33c57cae6b3e4431dc18b14f1b325b4
soorokim <rlatnfh99@gmail.com>
2024-03-24 11:35:38 +0900

    vault backup: 2024-03-24 11:35:38

---
+++ study/모던 자바스크립트 Deep Dive/t.q.js
@@ -0,0 +1,12 @@
+const arr = [1, 2, 3, 4];
+
+arr.length = 3;
+
+console.log(arr); // [1, 2, 3]
+
+// 현재 length 보다 큰 값을 할당하면 length값은 변경되지만 실제로 배열의 길이가 늘어나지 않는다.
+arr.length = 5;
+
+console.log(arr); // [1, 2, 3, , ]
+
+console.log(Object.getOwnPropertyDescriptors(arr));
