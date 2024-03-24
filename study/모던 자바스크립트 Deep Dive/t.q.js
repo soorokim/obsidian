0c56e68b44205bfa20fc58f876eb55e5acdcca2a
soorokim <rlatnfh99@gmail.com>
2024-03-24 14:09:21 +0900

    vault backup: 2024-03-24 14:09:21

--- study/모던 자바스크립트 Deep Dive/t.q.js
+++ study/모던 자바스크립트 Deep Dive/t.q.js
@@ -1,12 +1,20 @@
-const arr = [1, 2, 3, 4];
+// const arr = [1, 2, 3, 4];
 
-arr.length = 3;
+// arr.length = 3;
 
-console.log(arr); // [1, 2, 3]
+// console.log(arr); // [1, 2, 3]
 
-// 현재 length 보다 큰 값을 할당하면 length값은 변경되지만 실제로 배열의 길이가 늘어나지 않는다.
-arr.length = 5;
+// // 현재 length 보다 큰 값을 할당하면 length값은 변경되지만 실제로 배열의 길이가 늘어나지 않는다.
+// arr.length = 5;
 
-console.log(arr); // [1, 2, 3, , ]
+// console.log(arr); // [1, 2, 3, , ]
 
-console.log(Object.getOwnPropertyDescriptors(arr));
+// console.log(Object.getOwnPropertyDescriptors(arr));
+
+const a = new Array(5);
+
+a[0] = 11;
+
+const b = a.map((t) => t + 1);
+
+console.log(b);
